477337f784849181ff73707a1f76db090a625c06
Albin Reji <albinrejim30@gmail.com>
2026-08-13 14:11:57 +0530

    feat(product-service): add Keycloak JWT security configuration

---
+++ product-service/src/main/java/com/aegis/product_service/config/SecurityConfig.java
@@ -0,0 +1,56 @@
+package com.aegis.product_service.config;
+
+import com.aegis.product_service.security.KeycloakJwtAuthenticationConverter;
+import com.aegis.product_service.security.SecurityAccessDeniedHandler;
+import com.aegis.product_service.security.SecurityAuthenticationEntryPoint;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final SecurityAuthenticationEntryPoint securityAuthenticationEntryPoint;
+    private final SecurityAccessDeniedHandler securityAccessDeniedHandler;
+
+    private final KeycloakJwtAuthenticationConverter keycloakJwtAuthenticationConverter;
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) {
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/actuator/health",
+                                "/swagger-ui/**",
+                                "/swagger-ui.html",
+                                "/v3/api-docs/**")
+                        .permitAll()
+
+                        .requestMatchers("/api/v1/admin/**")
+                        .hasRole("ADMIN")
+
+
+                        .requestMatchers("/api/v1/products/**",
+                                        "/api/v1/categories/**")
+                        .hasRole("USER")
+//                     admin privilege apis
+
+                        .anyRequest()
+                        .authenticated())
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(
+                        jwt -> jwt.jwtAuthenticationConverter(keycloakJwtAuthenticationConverter))
+                        .authenticationEntryPoint(securityAuthenticationEntryPoint)
+                        .accessDeniedHandler(securityAccessDeniedHandler)
+
+
+                );
+
+        return http.build();
+
+    }
+}
